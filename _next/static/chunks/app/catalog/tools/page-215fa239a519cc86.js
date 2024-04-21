79e54d60ad09e3df07577a01eeeee2a0f794a9ef
vishnevsky533 <vishnevsky533@gmail.com>
2024-04-21 13:56:17 +0300

    Updates

--- _next/static/chunks/app/catalog/tools/page-215fa239a519cc86.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[144,931,308,926,983,546,104],{8278:function(e,n,s){Promise.resolve().then(s.t.bind(s,1749,23)),Promise.resolve().then(s.t.bind(s,5250,23))}},function(e){e.O(0,[250,749,971,69,744],function(){return e(e.s=8278)}),_N_E=e.O()}]);
